efbbf69903b0c6e3db414da68fd32da9880c577b
Tor Arne Birkeland <torarne.94@gmail.com>
2026-06-12 16:27:07 +0200

    bugfix intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -22,8 +22,11 @@ export default function Intro() {
     >
       <div className="flex items-center justify-center">
         <div className="relative">
-          <motion.div
+          {/* <motion.div
             initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }} */}
+          <motion.div
+            initial={false}
             animate={{ opacity: 1, scale: 1 }}
             transition={{
               type: "tween",
@@ -57,11 +60,11 @@ export default function Intro() {
         </div>
       </div>
 
-      <motion.h1
+      {/* <motion.h1
         className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
-      >
+        animate={{ opacity: 1, y: 0 }} */}
+      <motion.h1 initial={false} animate={{ opacity: 1, y: 0 }}>
         <span className="font-bold">
           Hi! I'm Tor.
           <br />
@@ -79,12 +82,16 @@ export default function Intro() {
         </span>
         .
         <br />
-        <p className="mt-6">Welcome to my website!</p>
+        <span className="mt-6 block">Welcome to my website!</span>
       </motion.h1>
 
-      <motion.div
+      {/* <motion.div
         className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
         initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }} */}
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={false}
         animate={{ opacity: 1, y: 0 }}
         transition={{
           delay: 0.1,
